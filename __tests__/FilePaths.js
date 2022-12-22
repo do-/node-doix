c17686b25cffc686f06a81241d3386496677e0ee
Dmitry Ovsyanko <do_@pochta.ru>
2022-12-22 18:19:59 +0300

    + FilePaths

---
+++ __tests__/FilePaths.js
@@ -0,0 +1,42 @@
+const Path = require ('path')
+const {DirPaths, FilePaths} = require ('..')
+
+const p = Path.join (__dirname, 'data', 'root1', 'back', 'lib', 'Model', 'dw', '2_entities')
+
+test ('files', () => {
+	
+	expect (() => {FilePaths ().next ()}).toThrow ()
+	
+	expect (() => {FilePaths ({dir: -1}).next ()}).toThrow ()
+
+	expect ([...FilePaths ({
+			dir: DirPaths ({
+				root: ['root1', 'root2'].map (i => Path.join (__dirname, 'data', i)),
+				filter: (s, a) => a.at (-2) === 'dw'
+			})
+		}) 
+	]).toHaveLength (4)
+
+	expect ([...FilePaths ({
+			dir: DirPaths ({
+				root: Path.join (__dirname, 'data', 'root2'),
+				filter: (s, a) => a.at (-2) === 'dw'
+			})
+		}) 
+	]).toHaveLength (1)
+
+	expect ([...FilePaths ({
+			dir: DirPaths ({
+				root: Path.join (__dirname, 'data', 'root2'),
+			}),
+			filter: () => false
+		}) 
+	]).toHaveLength (0)
+
+	expect ([...FilePaths ({
+			dir: Path.join (__dirname, 'data', 'root2'),
+			filter: () => false
+		}) 
+	]).toHaveLength (0)
+
+})
--- index.js
+++ index.js
@@ -2,4 +2,5 @@ module.exports = {
 	ObjectMerger: require ('./lib/ObjectMerger.js'),
 	ModuleMergingMap: require ('./lib/ModuleMergingMap.js'),
 	DirPaths: require ('./lib/DirPaths.js'),
+	FilePaths: require ('./lib/FilePaths.js'),
 }
