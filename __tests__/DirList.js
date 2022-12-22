a9330a162935c0d71eba957f9e9b5b5a34aa0981
Dmitry Ovsyanko <do_@pochta.ru>
2022-12-22 21:28:39 +0300

    + DirList

---
+++ __tests__/DirList.js
@@ -0,0 +1,69 @@
+const fs = require ('fs')
+const Path = require ('path')
+const {DirList} = require ('..')
+
+const r = () => ['root1', 'root2'].map (i => Path.join (__dirname, 'data', i))
+
+const N = 35
+
+const x = Path.join (__dirname, 'data', 'root1', 'x')
+
+function x_off () {
+	if (fs.existsSync (x)) fs.rmdirSync (x)
+}
+
+function x_on () {
+	if (!fs.existsSync (x)) fs.mkdirSync (x)
+}
+
+
+test ('bad', () => {
+
+	const dir = new DirList ()
+
+	expect (() => dir.paths).toThrow ()
+
+})
+
+test ('live', () => {
+
+console.log (x)
+	
+	const dir = new DirList ({
+		root: r (),
+	})
+
+	x_off ()
+
+	expect ([...dir.paths]).toHaveLength (N)
+
+	x_on ()
+
+	expect ([...dir.paths]).toHaveLength (N + 1)
+
+	x_off ()
+
+	expect ([...dir.paths]).toHaveLength (N)
+
+})
+
+test ('!live', () => {
+
+	const dir = new DirList ({
+		root: r (),
+		live: false,
+	})
+
+	x_off ()
+
+	expect ([...dir.paths]).toHaveLength (N)
+
+	x_on ()
+
+	expect ([...dir.paths]).toHaveLength (N)
+
+	x_off ()
+
+	expect ([...dir.paths]).toHaveLength (N)
+
+})
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 module.exports = {
 	ObjectMerger: require ('./lib/ObjectMerger.js'),
 	ModuleMergingMap: require ('./lib/ModuleMergingMap.js'),
+	DirList: require ('./lib/DirList.js'),
 	DirPaths: require ('./lib/DirPaths.js'),
 	FilePaths: require ('./lib/FilePaths.js'),
 }
