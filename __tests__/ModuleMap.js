aeaccaf13dc67c309c8bea66afb6aabedeeba7f4
Dmitry Ovsyanko <do_@pochta.ru>
2022-12-24 23:17:00 +0300

    ModuleMap rewritten with ModuleLoader

--- __tests__/ModuleMap.js
+++ __tests__/ModuleMap.js
@@ -14,19 +14,6 @@ class MyObjectMerger extends ObjectMerger {
 	}
 }
 
-const mu1 = () => ({
-	type: 'users',
-	get_item: () => ({id: 1})
-})
-
-const mu2 = () => ({
-	select: () => [1, 2].map (id => ({id}))
-})
-
-const mr = () => ({
-	select: () => ([{name: 'operator'}])
-})
-
 test ('constructor', () => {
 
 	expect (() => {
@@ -80,78 +67,6 @@ test ('constructor', () => {
 			ext: 0,
 		})
 	}).toThrow ()
-
-})
-
-test ('set', () => {
-
-	const m = new ModuleMap ({
-		dir,
-	})
-
-	expect (() => {
-		m.set (1, mu1 ())
-	}).toThrow ()
-
-	expect (() => {
-		m.set ('users', 1)
-	}).toThrow ()
-
-	expect (() => {
-		m.set ('users', null)
-	}).toThrow ()
-
-	expect (() => {
-		m.set ('users', [mu1 (), mu2 ()])
-	}).toThrow ()
-
-	m.set ('users', mu1 ())
-	m.set ('users', mu2 ())
-
-	m.set ('roles', mr ())
-
-	expect (m.size).toBe (2)
-	
-	const users = m.get ('users')
-	const roles = m.get ('roles')
-
-	expect (users.type).toBe ('users')
-
-	expect (users.get_item ()).toStrictEqual ({id: 1})
-
-	expect (users).toHaveProperty ('select')
-
-	expect (users.select ()).toStrictEqual ([users.get_item (), {id: 2}])
-
-	expect (roles.select ()).toStrictEqual ([{name: 'operator'}])
-
-})
-
-
-test ('custom merge', () => {
-
-	const m = new ModuleMap ({
-		dir,
-		merger: new MyObjectMerger ()
-	})
-
-	m.set ('users', mu1 ())
-	m.set ('users', mu2 ())
-
-	m.set ('roles', mr ())
-
-	expect (m.size).toBe (2)
-	
-	const users = m.get ('users')
-	const roles = m.get ('roles')
-
-	expect (users.type).toBe ('users')
-
-	expect (users.get_item ()).toStrictEqual ({id: 1})
-	
-	expect (users).not.toHaveProperty ('select')
-
-	expect (roles.select ()).toStrictEqual ([{name: 'operator'}])
 
 })
 
@@ -232,11 +147,33 @@ test ('load complete', () => {
 
 test ('get complete', () => {
 
-	const m = new ModuleMap ({dir})
+	const m = new ModuleMap ({dir, watch: true})
 			
 	m.merger.on ('complete', (o, name) => o.name = name)
 	
 	const name = 'tb_houses'
+
+	expect (m.get (name)).toStrictEqual ({
+		name,
+		columns: {
+			root1_oltp: 1,
+			root1_crm_oltp: 1,
+			root1_hr_oltp: 1,
+			root2_hr_oltp: 1
+		}
+	})
+
+	expect (m.get (name)).toStrictEqual ({
+		name,
+		columns: {
+			root1_oltp: 1,
+			root1_crm_oltp: 1,
+			root1_hr_oltp: 1,
+			root2_hr_oltp: 1
+		}
+	})
+	
+	m.loader.mtimes.clear ()
 
 	expect (m.get (name)).toStrictEqual ({
 		name,
