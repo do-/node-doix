05814c1bf1945a4012a3e86f5e32e07292e2f1da
Dmitry Ovsyanko <do_@pochta.ru>
2022-12-24 22:41:54 +0300

    +ModuleLoader

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 module.exports = {
 	ObjectMerger: require ('./lib/ObjectMerger.js'),
 	ModuleMap: require ('./lib/ModuleMap.js'),
+	ModuleLoader: require ('./lib/ModuleLoader.js'),
 	DirList: require ('./lib/DirList.js'),
 	FilePaths: require ('./lib/FilePaths.js'),
 }
