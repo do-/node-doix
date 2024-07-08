9300cf4edbaede4c3ecb63106763a1c3b25dc5c0
Dmitry Ovsyanko <do_@pochta.ru>
2024-07-08 09:50:44 +0300

    Application +pools type validation

--- __tests__/Application.js
+++ __tests__/Application.js
@@ -1,6 +1,5 @@
 const Path = require ('path')
-const {Application, MethodSelector, JobLifeCycleTracker, JobSource} = require ('..')
-const { isGeneratorFunction } = require('util/types')
+const {Application, MethodSelector, JobLifeCycleTracker, JobSource, ResourcePool} = require ('..')
 
 const modules = {dir: {root: Path.join (__dirname, 'data', 'root3')}}
 
@@ -11,11 +10,12 @@ test ('constructor', () => {
 	expect (() => {new Application ()}).toThrow (TypeError)
 	expect (() => {new Application ({})}).toThrow ()
 	expect (() => {new Application ({modules, foo: 1})}).toThrow ()	
+	expect (() => {new Application ({modules, pools: {db: {connectionString: '...'}}})}).toThrow ('ResourcePool')	
 	expect (() => {new Application ({modules, methodSelector: 0})}).toThrow ()	
 	expect (new Application ({modules, methodSelector: undefined})).toBeInstanceOf (Application)
 	expect (new Application ({modules, methodSelector: new MS ()})).toBeInstanceOf (Application)
 
-	const app = new Application ({modules})
+	const app = new Application ({modules, pools: {}})
 	
 	expect (app).toBeInstanceOf (Application)
 	expect (app.methodSelector).toBeInstanceOf (MethodSelector)
