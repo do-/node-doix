34a6ec78b11d5c247fe0b6c743887db456fe48a9
Dmitry Ovsyanko <do_@pochta.ru>
2024-08-22 13:47:43 +0300

    MethodSelector: {rq} -> rq

--- __tests__/MethodSelector.js
+++ __tests__/MethodSelector.js
@@ -6,17 +6,15 @@ test ('getModuleName', () => {
 		
 	expect (() => m.getModuleName ()).toThrow (TypeError)
 	expect (() => m.getModuleName (0)).toThrow (TypeError)
-	expect (() => m.getModuleName ({rq: 0})).toThrow (TypeError)
-	expect (() => m.getModuleName ({rq: {type: NaN}})).toThrow (TypeError)
+	expect (() => m.getModuleName ({type: NaN})).toThrow (TypeError)
 
 	expect (m.getModuleName ({})).toBe (null)
-	expect (m.getModuleName ({rq: {}})).toBe (null)
 
 	for (const type of [null, undefined])
-		expect (m.getModuleName ({rq: {type}})).toBe (null)
+		expect (m.getModuleName ({type})).toBe (null)
 
 	for (const type of ['users'])
-		expect (m.getModuleName ({rq: {type}})).toBe (type)
+		expect (m.getModuleName ({type})).toBe (type)
 
 })
 
@@ -25,11 +23,11 @@ test ('getMethodName', () => {
 	const m = new MethodSelector ()
 	const type = 'users'
 
-	expect (m.getMethodName ({rq: {type}})).toBe ('select_users')
-	expect (m.getMethodName ({rq: {type, id: 1}})).toBe ('get_item_of_users')
-	expect (m.getMethodName ({rq: {type, part: 'vocs', id: 1}})).toBe ('get_vocs_of_users')
-	expect (m.getMethodName ({rq: {type, part: 'vocs'}})).toBe ('get_vocs_of_users')
-	expect (m.getMethodName ({rq: {type, action: 'create', id: 1}})).toBe ('do_create_users')
-	expect (m.getMethodName ({rq: {type, action: 'create'}})).toBe ('do_create_users')
+	expect (m.getMethodName ({type})).toBe ('select_users')
+	expect (m.getMethodName ({type, id: 1})).toBe ('get_item_of_users')
+	expect (m.getMethodName ({type, part: 'vocs', id: 1})).toBe ('get_vocs_of_users')
+	expect (m.getMethodName ({type, part: 'vocs'})).toBe ('get_vocs_of_users')
+	expect (m.getMethodName ({type, action: 'create', id: 1})).toBe ('do_create_users')
+	expect (m.getMethodName ({type, action: 'create'})).toBe ('do_create_users')
 
 })
